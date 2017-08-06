fc4f08decb4d4e36097fea8191fb9434b2517459
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-08-06 15:20:38 -0700

    added two pages and images

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,42 @@
-import static spark.Spark.*
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        get("/", (request, response) --> "Hello Friend");
+        staticFileLocation("/public");
+        get("/", (request, response) ->
+                "<!DOCTYPE html>" +
+                "<html>" +
+                "<head>" +
+                "<title>Hello Friend!</title>" +
+                "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+                "</head>" +
+                "<body>" +
+                "<h1>Hello From Afar</h1>" +
+                "<p>Dear Friend,</p>" +
+                "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
+                "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
+                "<p>But I like programming a lot, so I've got that going for me. </p>" +
+                "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
+                "<p>Cheers,</p>" +
+                "<p>Travel Enthusiast Jane</p>" +
+                "</body>" +
+                "</html>");
+
+        get("/favorite_photos", (request, response) ->
+                "<!DOCTYPE html>" +
+                    "<html>" +
+                    "<head>" +
+                        "<title>Hello Friend!</title>" +
+                        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+                    "</head>" +
+                    "<body>" +
+                        "<h1>Favorite Traveling Photos</h1>" +
+                            "<ul>" +
+                                "<li><img src='/images/mountain.jpg' alt='A photo of a mountain.'/></li>" +
+                                "<li><img src='/images/trees.jpg' alt='A photo of forest trees.'/></li>" +
+                            "</ul>" +
+                    "</body>" +
+                    "</html>"
+        );
     }
 }
